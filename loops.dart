45f48c18456c22d64c2d177fac65327d42f64478
infiniteoverflow <aswingopinathan1871@gmail.com>
2019-04-23 22:09:06 +0530

    Loops

---
+++ loops.dart
@@ -0,0 +1,28 @@
+void main() {
+
+  /* Loops are of three types:
+  1. For loop
+  2. While loop
+  3. Do-While loop
+  */
+
+  //case 1: For Loop
+  for(int i=0;i<10;i++) {
+    print("$i");
+  }
+
+  //case 2: While Loop
+  var i=0;
+  while(i<10) {
+    print(i);
+    i++;
+  }
+
+  //case 3: Do While Loop
+  i=0;
+  do {
+    print(i);
+    i++;
+  } while(i!=10);
+
+}
