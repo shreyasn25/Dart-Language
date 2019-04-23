60e9fd6de768363026e564d6d5ee992816b99099
infiniteoverflow <aswingopinathan1871@gmail.com>
2019-04-23 22:03:38 +0530

    if-else

---
+++ if_else.dart
@@ -0,0 +1,20 @@
+void main() {
+  
+  int salary = 25000;
+  if(salary>20000) {
+    print("Salary is good");
+  } else {
+    print("You have to work harder");
+  }
+
+
+  /*
+  SYNTAX for If-else Statement
+    if(condition) {
+      statements
+    } else {
+      statements
+    }
+  */
+
+}
